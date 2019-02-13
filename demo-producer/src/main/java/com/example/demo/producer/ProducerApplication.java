b49a005d5ddb10c755c4cd377fb686c3e7e34782
S Ehmann <sehmann@pivotal.io>
2019-02-13 11:30:38 -0600

    Move producer/consumer into seperate projects; producer test works

---
+++ demo-producer/src/main/java/com/example/demo/producer/ProducerApplication.java
@@ -0,0 +1,33 @@
+package com.example.demo.producer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController("/")
+@EnableBinding(MessageSource.class)
+public class ProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerApplication.class, args);
+	}
+
+	@Autowired
+	MessageSource messageSource;
+
+	@PostMapping(value = "/makeMessage")
+	public String enqueue(@RequestBody String body) {
+		System.out.println(body);
+		messageSource.messageChannel().send(MessageBuilder.withPayload(body).build());
+		return "message sent";
+	}
+}
+
--- demo/src/main/java/com/example/demo/producer/MessageSource.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.producer;
-
-import org.springframework.cloud.stream.annotation.Output;
-import org.springframework.messaging.MessageChannel;
-
-public interface MessageSource {
-    @Output("foodOrdersChannel")
-    MessageChannel messageChannel();
-}
