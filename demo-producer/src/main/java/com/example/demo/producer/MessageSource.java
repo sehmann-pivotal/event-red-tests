e6e6a3df6c694e160fc72ecc7ab4eff862af5620
Laura Zukoski <LZ18@ntrs.com>
2019-02-14 12:07:24 -0600

    Remove demo2; clean up tests for consumer/producer

--- demo-producer/src/main/java/com/example/demo/producer/MessageSource.java
+++ demo-producer/src/main/java/com/example/demo/producer/MessageSource.java
@@ -4,6 +4,6 @@ import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.messaging.MessageChannel;
 
 public interface MessageSource {
-    @Output("foodOrdersChannel")
+    @Output("llama")
     MessageChannel messageChannel();
 }
